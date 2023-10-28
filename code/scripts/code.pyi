9315412ee3a4f77d7c4bc5b6df26d99c41857ab4
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-10-27 20:56:13 -0700

    Create code.pyi

---
+++ code/scripts/code.pyi
@@ -0,0 +1,23 @@
+#include <info.json>
+#include <script.gradle>
+
+name1("namespace", defList1, nameData-DEF01);
+{
+def MakeInstance(instanceName, instanceVersion, instanceDirectory);
+def GoogleInstance(googleInstanceName, googleInstanceVersion, googleInstanceDirectory);
+def GoogleCloud(CloudInstanceName, CloudInstanceVersion, CloudInstanceDirectory);
+def MinecraftCode(MinecraftCode, CodeFiles, JavaFiles, Directories);
+}
+ #$int 5FFFFE
+#java
+#%java%minecraft%temp$crumb20
+#obfuscated_maps
+#obfuscateJavaCodeInObfuscationMaps
+#$int 0000001C16F5E92B
+#$int break 6e44af1
+#this = case Dot.JavaData
+#java.dot
+#break
+#break data java.uof
+#echo ;'EOF'
+#EOF.sh
